5828ac1ca6e0312e318ca79cbf82384f57134d59
ManNjoro <elijohnmwoho@gmail.com>
2022-06-20 11:36:41 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+function greet(){
+    let nameInput = document.getElementById('nameInput');
+    let nameField = document.getElementById('nameField');
+
+    nameField.innerHTML += "Hello " + nameInput.value  +", welcome to the bootcamp";
+}
